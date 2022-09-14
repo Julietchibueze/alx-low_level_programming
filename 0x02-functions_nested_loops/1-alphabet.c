036b339528a86288c1ca25239c769c7b7783001d
Juliet Chibueze <chibuezejuliet71@gmail.com>
2022-09-14 04:18:58 -0700

    don1

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * main - print letters
+ *
+ * Return: always 0
+ */
+
+int main(void)
+{
+	char c;
+
+	for (c = 'a'; c <= 'z'; c++)
+	{
+		_putchar(c);
+	}
+	_putchar('\n');
+}
